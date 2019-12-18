2e2d69d96548a1fab8c3fe34220bc62eb18ff339
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-12-18 16:27:38 -0700

    Passed insert() test

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,5 +1,5 @@
-import sys
-sys.path.append('../stack_dll')
+# import sys
+# sys.path.append('../stack_dll')
 # from dll_queue import Queue
 # from stack_dll import Stack
 
@@ -16,39 +16,36 @@ class BinarySearchTree:
         # print("Node",current_node,"Node Value" ,current_node.value)
 
         # If inserting we must already have a tree/root <-- ?
-        #value = self.value
-
+        # self.right and self.left are pointers
+        
         # SIMPLIFY:
-        if value > self.value:                            # If value is less than self.value
-            if self.left == None:                         # If left node is empty
-                return self.left.insert(value)                   # Insert the value
-                # self.left = BinarySearchTree(value)        # Recursion to keep searching until empty node is found
+        if  self.value > value:                       # If value is less than self.value
+            if self.left == None:                     # If left node is empty
+                 self.left = BinarySearchTree(value)  # Recursion - keep searching until empty node is found
             else:
-                # return self.left.insert(value)
-                self.left = BinarySearchTree(value)      # Recursion:  keep searching until empty node is found
+                return self.left.insert(value)        # Recursion - passing the value back into the insert() to check if again
 
-        elif value >= self.value:
+        if  self.value <= value:
             if self.right == None:
                 self.right = BinarySearchTree(value)
-                # return self.right.insert(value)
             else:
-                # self.right = BinarySearchTree(value)
-                return self.right.insert(value)
+                return self.right.insert(value) 
 
-
-    
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
         # if target == self.value, return it,
         # go left or right based on smaller or bigger
+        # if target == self. value:
+
         pass
 
     # Return the maximum value found in the tree
     def get_max(self):
         #  if right exists, go right
         # otherwise return self.value
+        #  You can only go right for largest, and you know you are at the end of the tree when the node is a leaf and there are no more children.
         pass
 
     # Call the function `cb` on the value of each node
